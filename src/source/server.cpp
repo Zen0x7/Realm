57251b57161a072b0cf213802db566c5ca92acea
Ian Torres <iantorres@outlook.com>
2024-08-17 19:36:54 -0400

    Sources and headers in separated folders.

---
+++ src/source/server.cpp
@@ -0,0 +1,13 @@
+#include <server.h>
+#include <session.h>
+
+void server::do_accept() {
+    acceptor_.async_accept(socket_,
+                           [this](boost::system::error_code ec) {
+                               if (!ec) {
+                                   std::make_shared<session>(std::move(socket_))->start();
+                               }
+
+                               do_accept();
+                           });
+}
---
+++ src/source/session.cpp
@@ -0,0 +1,21 @@
+#include <session.h>
+
+void session::do_read()  {
+    auto self(shared_from_this());
+    socket_.async_read_some(boost::asio::buffer(data_, max_length),
+                            [this, self](boost::system::error_code ec, std::size_t length) {
+                                if (!ec) {
+                                    do_write(length);
+                                }
+                            });
+}
+
+void session::do_write(std::size_t length) {
+    auto self(shared_from_this());
+    async_write(socket_, boost::asio::buffer(data_, length),
+                [this, self](boost::system::error_code ec, std::size_t /*length*/) {
+                    if (!ec) {
+                        do_read();
+                    }
+                });
+}
