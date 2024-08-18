ec9978480b806a0fe46e49136861ac8400cbcd44
Ian Torres <iantorres@outlook.com>
2024-08-18 01:00:21 -0400

    Broadcasting added.

--- src/include/state.h
+++ src/include/state.h
@@ -9,7 +9,9 @@ class session;
 
 class state : public std::enable_shared_from_this<state> {
     std::unordered_map<std::string, session*> sessions_;
+    std::mutex mutex_;
 public:
     void insert(session * session);
     void remove(const session * session);
+    void broadcast(std::string data);
 };
--- src/state.cpp
+++ src/state.cpp
@@ -3,9 +3,27 @@
 #include <session.h>
 
 void state::insert(session * session) {
+    std::lock_guard lock(mutex_);
     sessions_.insert({ session->get_id(), session });
 }
 
 void state::remove(const session * session) {
+    std::lock_guard lock(mutex_);
     sessions_.erase(session->get_id());
+}
+
+void state::broadcast(std::string data) {
+    auto const ss = std::make_shared<std::string const>(std::move(data));
+
+    std::vector<std::weak_ptr<session>> v;
+    {
+        std::lock_guard lock(mutex_);
+        v.reserve(sessions_.size());
+        for (auto p: sessions_)
+            v.emplace_back(p.second->weak_from_this());
+    }
+
+    for (auto const & wp : v)
+        if (auto sp = wp.lock())
+            sp->send(ss);
 }
